f6c30b9f1ddedcf92113c7cf140a7e9de1b583f8
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-04-23 11:03:58 -0500

    "finished the general layout of the class files (cirlce.js, square.js,triangle,js and shapes.js) now just need to add the specific rendering method for each"

---
+++ index.js
@@ -0,0 +1,41 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "characters",
+      message: "Enter up to 3 characters that you want for your logo",
+      validate: function (input) { 
+        return input.length <= 3
+      }
+    },
+    {
+      type: "list",
+      message: "Choose the shape you want your logo to be",
+      name: "shapes",
+      choices: ["circle", "triangle", "square"],
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter a color keyword (or a hexadecimal number) for your text color:',
+     
+      },
+      {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter a color keyword (or a hexadecimal number) for your shape color:',
+     
+      },
+  ])
+  .then((data) => {
+    console.log(data);
+    const filename = `${data.name.toLowerCase().split(" ").join("")}.json`;
+    //Farley Wittles 
+    //farleywittles.json
+    fs.writeFile(filename, JSON.stringify(data, null, "\t"), (err) =>
+      err ? console.log(err) : console.log("Success!")
+    );
+  });
