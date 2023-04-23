04b14685165f4bfb3d2ba72e22b702a55b360862
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-04-23 16:03:32 -0500

    test

--- tests/triangle.test.js
+++ tests/triangle.test.js
@@ -5,29 +5,26 @@ const { describe, test } = require('jest');
 
 
 // Import the Triangle class from the file where it is defined.
-const Triangle = require('../lib/Triangle');
+// Import the Circle class from the file where it is defined.
+const Circle = require('../lib/circle');
 
 
-
-
-
-
-// A test suite for the Square class.
-describe('Square', () => {
+// A test suite for the Circle class.
+describe('Circle', () => {
     describe('Instantiate', () => {
-  // A test case to check if the Square class can be instantiated with the required parameters.
-  test('should create a new Square instance with the given parameters', () => {
-    const Square = new Square('white', 'red', 'A');
-    expect(Square).toBeInstanceOf(Square);
-    expect(Square.textColor).toBe('white');
-    expect(Square.shapeColor).toBe('red');
-    expect(Square.characters).toBe('A');
+  // A test case to check if the Circle class can be instantiated with the required parameters.
+  test('should create a new Circle instance with the given parameters', () => {
+    const circle = new Circle('white', 'red', 'A');
+    expect(circle).toBeInstanceOf(Circle);
+    expect(circle.textColor).toBe('white');
+    expect(circle.shapeColor).toBe('red');
+    expect(circle.characters).toBe('A');
   });
 });
-  // A test case to check if the Square class can render a Square with the given parameters.
+  // A test case to check if the Circle class can render a circle with the given parameters.
   describe("renderSpecificShape()", () => {
-  test('should render a Square with the given parameters', () => {
-    const Square = new Square('white', 'red', 'A');
+  test('should render a circle with the given parameters', () => {
+    const circle = new Circle('white', 'red', 'A');
     const expectedResult = `
     <ellipse cx="150" cy="100" rx="150" ry="100"  fill="red" />
       <text x="125" y="150" font-size="24" text-anchor="middle" fill="white">
@@ -35,7 +32,7 @@ describe('Square', () => {
       </text>
     `;
   
-    expect(Square.renderSpecificShape().replace(/\s+/g, '')).toBe(expectedResult.replace(/\s+/g, ''));
+    expect(circle.renderSpecificShape().replace(/\s+/g, '')).toBe(expectedResult.replace(/\s+/g, ''));
   });
 });
 });
