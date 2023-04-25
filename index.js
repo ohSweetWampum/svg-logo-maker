ab5f4863fedb56343ccb12cd22c730ffff3ebdfa
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-04-25 01:58:01 -0500

    "added some comments"

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+// Import required modules
 const inquirer = require('inquirer');
 const fs = require('fs');
-// import shape classes from shapes.js
 
-
+// Prompt the user for input using inquirer
 inquirer
   .prompt([
     {
@@ -17,67 +17,68 @@ inquirer
       type: "list",
       message: "Choose the shape you want your logo to be",
       name: "shapes",
-      choices: ["circle", "triangle", "square"],
+      choices: ["circle", "triangle", "square"], 
     },
     {
         type: 'input',
         name: 'textColor',
         message: 'Enter a color keyword (or a hexadecimal number) for your text color:',
-     
-      },
-      {
+    },
+    {
         type: 'input',
         name: 'shapeColor',
         message: 'Enter a color keyword (or a hexadecimal number) for your shape color:',
-     
-      },
+    },
   ])
- 
-
-
   .then((data) => {
+    console.log(data);
+    // Create a file name based on the input characters
     const filename = `${data.characters.toLowerCase().split(" ").join("")}.json`;
+
     
     fs.writeFile(filename, JSON.stringify(data, null, "\t"), (err) =>
       err ? console.log(err) : console.log("successful")
     );
-  
+
+    // Generate the SVG image with the user's input
     const svgImage = generateTheSVGImage(data.textColor, data.shapeColor, data.characters, data.shapes);
-   
-  
+    console.log(svgImage);
+
+    
     fs.writeFile(`./examples/${data.characters.toLowerCase().split(" ").join("")}.svg`, svgImage, (err) =>
       err ? console.log(err) : console.log("Generated logo.svg")
     );
   });
-  
 
-  //function to generate the SVG image
-  function generateTheSVGImage(textColor, shapeColor, characters, shape) {
-    let shapeSVG = '';
-    let textX, textY, textDY;
-  
-    switch (shape) {
-      case 'circle':
-        shapeSVG = `<circle cx="150" cy="100" r="80" fill="${shapeColor}" />`;
-        textX = "50%";
-        textY = "50%";
-        textDY = ".3em";
-        break;
-      case 'triangle':
-        shapeSVG = `<polygon points="150, 18 244, 182 56, 182" fill="${shapeColor}" />`;
-        textX = "50%";
-        textY = "75%";
-        textDY = ".3em";
-        break;
-      case 'square':
-        shapeSVG = `<rect x="50" y="0" width="200" height="200" fill="${shapeColor}" />`;
-        textX = "50%";
-        textY = "50%";
-        textDY = ".3em";
-        break;
-    }
-    
-    return `
+//  generate the SVG image based on user input
+function generateTheSVGImage(textColor, shapeColor, characters, shape) {
+  let shapeSVG = '';
+  let textX, textY, textDY;
+
+  // Determine the shape and set the SVG string for the shape
+  switch (shape) {
+    case 'circle':
+      shapeSVG = `<circle cx="150" cy="100" r="80" fill="${shapeColor}" />`;
+      textX = "50%";
+      textY = "50%";
+      textDY = ".3em";
+      break;
+    case 'triangle':
+      shapeSVG = `<polygon points="150, 18 244, 182 56, 182" fill="${shapeColor}" />`;
+      textX = "50%";
+      textY = "75%";
+      textDY = ".3em";
+      break;
+    case 'square':
+      shapeSVG = `<rect x="50" y="0" width="200" height="200" fill="${shapeColor}" />`;
+      textX = "50%";
+      textY = "50%";
+      textDY = ".3em";
+      break;
+  }
+
+  // Return the final SVG string with the shape and text
+  return `
     <svg viewBox="0 0 300 200" version="1.1" xmlns="http://www.w3.org/2000/svg">
       ${shapeSVG}
       <text x="${textX}" y="${textY}" dy="${textDY}" font-size="60" text-anchor="middle" fill="${textColor}">
@@ -85,7 +86,4 @@ inquirer
       </text>
     </svg>
   `;
-  
-    
-  }
-  
+}
